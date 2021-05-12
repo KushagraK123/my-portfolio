fb1be58ad7657f5b8b9734008dbaeaf1cbf81057
Kushagra Kasliwal <kushagrakasliwal47@gmail.com>
2021-05-12 18:02:09 +0530

    Add login navigation from home page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Bio } from '../bio/bio.model';
 import { BioService } from '../bio/bio.service';
@@ -24,7 +25,7 @@ export class FooterComponent implements OnInit {
   };
   
   currentYear = 2021;
-  constructor(private bioService: BioService) { 
+  constructor(private bioService: BioService, private router: Router) { 
     this.currentYear = new Date().getFullYear();
   }
 
@@ -42,6 +43,10 @@ export class FooterComponent implements OnInit {
   smoothScroll = function(id: string) {
     var scrollContainer = document.getElementById(id)!!;
     scrollContainer.scrollIntoView({behavior:"smooth"});
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
 
 }
