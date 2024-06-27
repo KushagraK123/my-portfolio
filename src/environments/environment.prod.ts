8dd2f07c9d48fbd3f4fe4e729377f0d77a6195f6
kushagra <kushagra@Kushagras-MacBook-Air.local>
2024-06-27 13:48:03 +0530

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,1 @@
-export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDXLH33tGoWqi6YM8ZUWNnXivauiHjRQ1M",
-    authDomain: "my-portfolio-e31d4.firebaseapp.com",
-    projectId: "my-portfolio-e31d4",
-    storageBucket: "my-portfolio-e31d4.appspot.com",
-    messagingSenderId: "765738492598",
-    appId: "1:765738492598:web:8cc7c7cfbf215f5099bc17",
-    measurementId: "G-D08DMPTE1R"
-  },
-  apiUrl: "https://kushagra-portfolio-backend.herokuapp.com/api"
 
-  
-};
